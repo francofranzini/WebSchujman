d2880a02e52df29ac05d56663d7165db55d82719
Franco Franzini <francofranzini12@gmail.com>
2023-05-15 16:52:03 -0300

    "claves.php y js agregados"

--- claves.php
+++ claves.php
@@ -1,15 +1,39 @@
 <?php
-$host = "localhost";
-$user = "nueve";
-$pass = "9999nueve9";
-$base = "nueve";
 
-$con = mysqli_connect($host, $user, $pass);
+//establecemos conexion con el servidor
+$HOST = "ipdelserver";
+$USER = "nueve";
+$PASS = "9999nueve9";
+$con = mysqli_connect ($HOST ,$USER , $PASS );
 
 if (! $con ) {
-die ("Error de Conexion (" .mysqli_connect_errno () . ") "
-. mysqli_connect_error () );
+    die ("Error de Conexion (" . mysqli_connect_errno () . ") ".mysqli_connect_error () );
+    }
 
-}
+mysqli_close( $con );
+
+//creamos la base de datos
+$db_name = "IPS ";
+$charset_name = " UTF8 ";
+$collation_name = " utf8_bin ";
+$sql = " CREATE DATABASE IF NOT EXISTS $db_name CHARACTER SET $charset_name
+COLLATE $collation_name ";
+
+if (! $result = mysqli_query ($con , $sql )) die( mysqli_error ( $con ));
+
 mysqli_close ( $con ) ;
+
+
+//nos conectamos con la base de datos
+$HOST = "ipdelservidor";
+$USER = "nueve";
+$PASS = "9999nueve9";
+$DB = "IPS";
+$con = mysqli_connect ($HOST ,$USER ,$PASS , $DB );
+if (! $con ) {
+    die ("Error de Conexion (".mysqli_connect_errno().") ".mysqli_connect_error () );
+}
+
+
+
 ?>
