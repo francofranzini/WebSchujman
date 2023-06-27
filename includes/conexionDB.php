02c969399d6381a4625f57c9de9a0aa7edc90214
BautiRenaudo9-12 <renaudobautista@gmail.com>
2023-06-26 21:37:23 -0300

    listorti

--- includes/conexionDB.php
+++ includes/conexionDB.php
@@ -6,7 +6,7 @@ $DB = "nueve";
 //
 //nos conectamos con la base de datos
 //
-$con = mysqli_connect ($HOST ,$USER ,$PASS , $DB );
+$con = mysqli_connect($HOST ,$USER ,$PASS , $DB );
 if (!$con) {
     die("Error de Conexion (" . mysqli_connect_errno() . ") " . mysqli_connect_error());
 }
